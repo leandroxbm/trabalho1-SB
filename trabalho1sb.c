16a69f2029b197b9279c73960d385dcfedc76196
Cayke <cayke_fjv@hotmail.com>
2014-05-03 00:54:24 -0300

    Update trabalho1sb.c

--- trabalho1sb.c
+++ trabalho1sb.c
@@ -2,46 +2,84 @@
 #include <stdio.h>
 #include <string.h>
 
-struct lista {
- char mnemonico[10];
- int operandos;
- int codigo;
- int tamanho;
+struct lista
+{
+    char mnemonico[100];
+    int operandos;
+    int codigo;
+    int tamanho;
 
- struct lista* prox;
+    struct lista* prox;
+};
+
+struct listaEQU
+{
+    char chave[100];
+    char valor[100];
+
+    struct listaEQU *prox;
 };
 
 typedef struct lista Lista;
+typedef struct listaEQU ListaEQU;
 
 Lista* inicializa (void);
 Lista* insere (Lista* , char[], int, int, int);
 void imprime (Lista*);
-Lista* criaListaInstrucao (Lista*);
-Lista* criaListaDiretiva (Lista*);
+Lista* criaListaInstrucao ();
+Lista* criaListaDiretiva ();
 void liberaLista (Lista*);
+FILE* abrirArquivoAsm();
+FILE * abrirArquivoPre();
+void  fecharArquivo(FILE *);
+void preProcessamento(FILE *,FILE *);
 
-int main (int argc, char *argv[]){
-    Lista *instrucao;
-    Lista *diretiva;
+int main (int argc, char *argv[])
+{
 
-    instrucao = criaListaInstrucao(instrucao);
-    imprime(instrucao);
+    ///--------------------------------CRIAR LISTAS
+    /*   Lista *instrucao;
+       Lista *diretiva;
 
-    diretiva = criaListaDiretiva(diretiva);
-    imprime(diretiva);
+       instrucao = criaListaInstrucao();
+       imprime(instrucao);
 
-    liberaLista(instrucao);
-    liberaLista(diretiva);
+       diretiva = criaListaDiretiva();
+       imprime(diretiva);
+
+       liberaLista(instrucao);
+       liberaLista(diretiva);
+    */
+    ///------------------------------ PRE PROCESSAMENTO
+    FILE *fp,*fp2;
+
+    //abrir o arquivo
+    fp = abrirArquivoAsm();
+    fp2 = abrirArquivoPre();
+    preProcessamento(fp,fp2);
+
+
+
+
+
+
+
+
+    fecharArquivo(fp);
+    fecharArquivo(fp2);
+    //---------------------------------------------------------
     return 0;
 }
 
 /* função de inicialização: retorna uma lista vazia */
-Lista* inicializa (void){
+Lista* inicializa (void)
+{
     return NULL;
 }
 
 /* inserção no início: retorna a lista atualizada */
-Lista* insere (Lista* lista, char mnemonico[], int operandos, int codigo, int tamanho){
+Lista* insere (Lista* lista, char mnemonico[], int operandos, int codigo, int tamanho)
+{
     Lista* novo = (Lista*) malloc(sizeof(Lista));
     strcpy(novo->mnemonico,mnemonico);
     novo->operandos = operandos;
@@ -52,24 +90,30 @@ Lista* insere (Lista* lista, char mnemonico[], int operandos, int codigo, int ta
 }
 
 /* função imprime: imprime valores dos elementos */
-void imprime (Lista* l){
+void imprime (Lista* l)
+{
     Lista* p; /* variável auxiliar para percorrer a lista */
-    for (p = l; p != NULL; p = p->prox){
+    printf ("\n");
+    for (p = l; p != NULL; p = p->prox)
+    {
         printf("%s\t%d\t%d\t%d\n", p->mnemonico, p->operandos, p->codigo, p->tamanho );
     }
 }
 
-void liberaLista (Lista* l){
+void liberaLista (Lista* l)
+{
     Lista* p = l;
-    while (p != NULL) {
+    while (p != NULL)
+    {
         Lista* t = p->prox; /* guarda referência para o próximo elemento*/
         free(p); /* libera a memória apontada por p */
         p = t; /* faz p apontar para o próximo */
     }
 }
 
-Lista* criaListaInstrucao (Lista* instrucao){
-
+Lista* criaListaInstrucao ()
+{
+    Lista * instrucao;
     instrucao = inicializa();
 
     instrucao = insere(instrucao, "stop",   0, 14, 1);
@@ -92,8 +136,9 @@ Lista* criaListaInstrucao (Lista* instrucao){
 }
 
 
-Lista* criaListaDiretiva (Lista* diretiva){
-
+Lista* criaListaDiretiva ()
+{
+    Lista * diretiva;
     diretiva = inicializa();
 
     diretiva = insere(diretiva, "if",      1, 0, 0);
@@ -103,5 +148,128 @@ Lista* criaListaDiretiva (Lista* diretiva){
     diretiva = insere(diretiva, "section", 1, 0, 0);
 
     return diretiva;
+
+}
+//ALTERAR NO FUTURO PARA ABRIR O ARQUIVO PELO NOME E RETORNAR O PONTEIRO FP
+FILE * abrirArquivoAsm()
+{
+    FILE *fp;
+
+    fp = fopen ("temp2.asm","r");
+    if (fp!=NULL)
+    {
+        printf("\nABRIU ARQUIVO\n");
+    }
+    else
+    {
+        printf ("\nArquivo nao abriu");
+        exit(1);
+    }
+
+    return fp;
+}
+
+FILE * abrirArquivoPre()
+{
+    FILE *fp;
+
+    fp = fopen ("teste.pre","w");
+    if (fp!=NULL)
+    {
+        printf("\nABRIU ARQUIVO\n");
+    }
+    else
+    {
+        printf ("\nArquivo nao abriu");
+        exit(1);
+    }
+
+    return fp;
+}
+
+//FECHAR ARQUIVO
+void  fecharArquivo(FILE *fp)
+{
+    fclose(fp);
+}
+
+//FAZER O PRE PROCESSAMENTO DO ARQUIVO ASM
+void preProcessamento(FILE *fp, FILE *fp2)
+{
+    long lSize;
+    char * buffer,*buffer2;
+    char palavra[100];
+    size_t result;
+    int i, j=0,k;
+    ListaEQU *equ;
+
+//   short controle=0; //variavel para controlar se houve linha antes do comentario, se nao houve nao e necessario \n
+
+    //pegar o tamanhgo do arquivo
+    fseek (fp , 0 , SEEK_END);
+    lSize = ftell (fp);
+
+    // alocar memoria
+    buffer = (char*) malloc (sizeof(char)*lSize);
+    buffer2 =(char*) malloc (sizeof(char)*lSize);
+
+
+    if (buffer == NULL||buffer2 == NULL)
+    {
+        exit (2);
+    }
+
+    // copiar arquivo para buffer
+    rewind (fp);
+    result = fread (buffer,1,lSize,fp);
+
+    //passar todas as letras para maiusculo
+    for(i=0; i<lSize; i++)
+    {
+        buffer[i]=toupper(buffer[i]);
+    }
+
+
+    //tirar comentarios
+    for( i=0; i<result; i++)
+    {
+        //se for ; e comentario,
+        if(buffer[i]==';')
+        {
+            //anda o ponteiro ate o fim da linha
+            while (buffer[i]!='\n')
+            {
+                i++;
+            }
+            if(1/*SE A PASSADA NO PRE JA E \N NAO DEVE ENTRAR NESSE IF*/) //  <--------------------------------------------------------------------------------
+            {
+                buffer2[j] = '\n';
+                j++;
+                fprintf(fp2,"\n");
+            }
+        }
+
+
+          else
+          {
+              buffer2[j] = buffer[i];
+              j++;
+              fprintf(fp2,"%c",buffer[i]);
+          }
+    }
+    buffer2[j]='\0';
+
+    puts(buffer2);
+
+    //procurar EQU
+
+
+
+
+
+
+    // desalocar
+    free (buffer);
+    free (buffer2);
 
 }
